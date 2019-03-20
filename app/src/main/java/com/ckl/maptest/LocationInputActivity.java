ed031304dd99abd291f8e0fda4e7fa474157acff
callumleung <callum.leung@gmail.com>
2019-03-20 09:59:57 +0000

    Initial commit

---
+++ app/src/main/java/com/ckl/maptest/LocationInputActivity.java
@@ -0,0 +1,14 @@
+package com.ckl.maptest;
+
+import android.app.Activity;
+import android.content.ContentValues;
+import android.view.View;
+import android.widget.Button;
+
+public class LocationInputActivity {
+
+    public void sendAddress(View view) {
+        //do something
+
+    }
+}
